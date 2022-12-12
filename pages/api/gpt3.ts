9aac3fcea61612beae1a905e6418257089adb84e
georgejeswin <jeswinmyladoor@gmail.com>
2022-12-13 02:43:59 +0530

    Created api, ui updates

--- pages/api/gpt3.ts
+++ pages/api/gpt3.ts
@@ -1,13 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
 
-type Data = {
-  name: string
-}
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const api = async (req: NextApiRequest, res: NextApiResponse) => {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: req.body.question.subject,
+    temperature: 0.6,
+    max_tokens: 2048,
+    // stream: true
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+};
+
+export default api;
